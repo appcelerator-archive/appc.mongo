f7e0b72cc967b27d7237d42ec31b31d1b71be3e7
Dawson Toth <dtoth@appcelerator.com>
2014-12-16 13:48:58 -0500

    https://jira.appcelerator.org/browse/API-230

--- app.js
+++ app.js
@@ -1,28 +1,9 @@
+/**
+ * NOTE: This file is simply for testing this connector and will not
+ * be used or packaged with the actual connector when published.
+ */
 var APIBuilder = require('appcelerator').apibuilder,
 	server = new APIBuilder();
-
-//--------------------- implement authorization ---------------------//
-
-// fetch our configured apikey
-var apikey = server.get('apikey');
-server.logger.info('APIKey is:', apikey);
-server.authorization = function APIKeyAuthorization(req, resp, next) {
-	if (!apikey) {
-		return next();
-	}
-	if (req.headers['apikey']) {
-		var key = req.headers['apikey'];
-		if (key == apikey) {
-			return next();
-		}
-	}
-	resp.status(401);
-	return resp.json({
-		id: "com.appcelerator.api.unauthorized",
-		message: "Unauthorized",
-		url: ""
-	});
-};
 
 var User = APIBuilder.Model.extend('user', {
 	fields: {
