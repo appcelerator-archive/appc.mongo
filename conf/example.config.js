96010ab0d993708342fcb5fd4fa2efca69be0388
Dawson Toth <dtoth@appcelerator.com>
2015-08-18 14:34:50 -0400

    [API-897] explicit modelAutogen: false
    https://jira.appcelerator.org/browse/API-897

--- conf/example.config.js
+++ conf/example.config.js
@@ -2,7 +2,12 @@ module.exports = {
 	connectors: {
 		'appc.mongo': {
 			url: 'mongodb://localhost/arrow',
-			generateModelsFromSchema: true // Generate models from your schema.
+
+			// Create models based on the schema that can be used in your API.
+			generateModelsFromSchema: true,
+
+			// Whether or not to generate APIs based on the methods in generated models. 
+			modelAutogen: false
 		}
 	}
 };
