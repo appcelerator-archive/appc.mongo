52f04cd0d3fcc5d4f0fa8196e93e3ee77bbc8531
Viktor Chobanov <chobanov10100@mail.bg>
2017-03-22 17:35:20 +0200

    [ARRSOF-135] Mongo Connector - Rewrite unit tests

--- test/unit/lifecycle/disconnect.js
+++ test/unit/lifecycle/disconnect.js
@@ -1,18 +1,15 @@
 const test = require('tap').test
+const disconnect = require('./../../../lib/lifecycle/disconnect').disconnect
 const server = require('./../../server')
-const fetchSchema = require('./../../../lib/schema/fetchSchema').fetchSchema
 const sinon = require('sinon')
 var ARROW
 var CONNECTOR
-
-const fetchSchemaSpy = sinon.spy(fetchSchema)
-const cbSpy = sinon.spy();
 
 test('### Start Arrow ###', function (t) {
   server()
     .then((inst) => {
       ARROW = inst
-			CONNECTOR = ARROW.getConnector('appc.mongo')
+      CONNECTOR = ARROW.getConnector('appc.mongo')
       t.ok(ARROW, 'Arrow has been started')
       t.end()
     })
@@ -21,28 +18,21 @@ test('### Start Arrow ###', function (t) {
     })
 })
 
-test('### fetchSchema ### - No DB case', function(t) {
+test('### disconnect ###', function (t) {
+  function func (opt) { }
+  const cbSpy = sinon.spy(func)
 
-	if(CONNECTOR.db)
-		const temp = CONNECTOR.db
-	
-	CONNECTOR.fetchSchema(cbSpy)
+  CONNECTOR.db = {
+    close: (next) => {
+      return next()
+    }
+  }
 
-	t.ok(cbSpy.calledOnce)
+  disconnect.apply(CONNECTOR, [cbSpy])
 
-	CONNECTOR.db = temp
+  t.ok(cbSpy.calledOnce)
 
-	t.end()
-})
-
-test('### fetchSchema ###', function (t) {
-	
-	
-	
-	
-	
-	
-	t.end()
+  t.end()
 })
 
 test('### Stop Arrow ###', function (t) {
