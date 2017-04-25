59850aa08934a987e47a97a100083e40dda8c82c
Meglena Lukanova <mlukanova@axway.com>
2017-04-25 16:40:28 +0300

    [ARRSOF-194] Integration tests don't pass.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 module.exports = function (grunt) {
-// Project configuration.
+  // Project configuration.
   grunt.initConfig({
     mochaTest: {
       test: {
@@ -11,8 +11,49 @@ module.exports = function (grunt) {
     }
   })
 
+  grunt.registerTask('createTestData', function () {
+    var done = this.async()
+    var mongoURL = 'mongodb://localhost/arrow'
+    var MongoDB = require('mongodb')
+    var MongoClient = MongoDB.MongoClient
+    MongoClient.connect(mongoURL, function didConnect (err, db) {
+      if (err) {
+        return console.error(err)
+      }
+      db.collection('super_post').insert([
+        { Hello: 'world!', Foo: 2 },
+        { Hello: 'sun!', Foo: 5 },
+        { divergentDocument: true },
+        { Hello: 'sky!', Foo: 7 },
+        { Hello: 'Earth!', Foo: 1 },
+        { Hello: 'birds!', Foo: 3 },
+        { How: 'are you today?!', Foo: 3 }
+      ])
+      db.close(done)
+    })
+  })
+
+  grunt.registerTask('cleanTestData', function () {
+    var done = this.async();
+    var mongoURL = 'mongodb://localhost/arrow'
+    var MongoDB = require('mongodb')
+    var MongoClient = MongoDB.MongoClient
+    MongoClient.connect(mongoURL, function didConnect(err, db) {
+      if (err) {
+        return console.error(err)
+      }
+      // Let's drop the database
+      db.dropDatabase(function (err) {
+        if (err) {
+          return console.error(err)
+        }
+        db.close(done)
+      })
+    })
+  })
+
   // Load grunt plugins for modules.
   grunt.loadNpmTasks('grunt-mocha-test')
 
-  grunt.registerTask('default', ['mochaTest'])
+  grunt.registerTask('default', ['createTestData', 'mochaTest', 'cleanTestData'])
 }
